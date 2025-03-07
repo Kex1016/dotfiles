2b08b53416b4d187c46f8464bab3251ae418d531
Kex1016 <cakes@haiiro.moe>
2025-03-07 01:30:59 +0100

    ags: Bar full functionality

--- .config/ags/widget/Bar.tsx
+++ .config/ags/widget/Bar.tsx
@@ -1,6 +1,8 @@
 import { App, Astal, Gtk, Gdk } from "astal/gtk3";
-import { bind, exec, interval, Variable } from "astal";
+import { bind, exec, GLib, interval, Variable } from "astal";
 import Tray from "gi://AstalTray";
+import Mpris from "gi://AstalMpris";
+import { Scrollable } from "astal/gtk3/widget";
 
 const time = Variable("").poll(1000, "date +%H:%M");
 
@@ -9,6 +11,10 @@ type Window = {
   title: string;
   active?: boolean;
 };
+
+function checkSameWindowList(a: Window[], b: Window[]) {
+  return JSON.stringify(a) === JSON.stringify(b);
+}
 
 function iconReplacements(icon: string) {
   const replacements = {
@@ -19,27 +25,63 @@ function iconReplacements(icon: string) {
   return replacements[icon as keyof typeof replacements] || icon;
 }
 
-export default function Bar(gdkmonitor: Gdk.Monitor) {
-  const { LEFT, RIGHT, BOTTOM } = Astal.WindowAnchor;
-  const trayItems = Variable<Tray.TrayItem[]>([]);
-  const trayItemsPrevious = Variable<Tray.TrayItem[]>([]);
+function AppLauncherButton() {
+  const appLauncherOpened = Variable(false);
+
+  function toggleAppLauncher() {
+    let appLauncherWindow;
+    try {
+      appLauncherWindow = App.get_window("applauncher");
+    } catch (e) {
+      // pass
+    }
+
+    if (appLauncherWindow) {
+      if (appLauncherWindow.is_visible()) {
+        appLauncherWindow.hide();
+        appLauncherOpened.set(false);
+      } else {
+        appLauncherWindow.show();
+        appLauncherOpened.set(true);
+      }
+    }
+  }
+
+  return (
+    <button
+      className={bind(appLauncherOpened).as((v) =>
+        v ? "appLauncher active" : "appLauncher"
+      )}
+      onClicked={toggleAppLauncher}
+      halign={Gtk.Align.START}
+    >
+      <icon icon={GLib.get_os_info("LOGO") || "view-grid-symbolic"} />
+    </button>
+  );
+}
+
+function MusicPlayer() {
+  const mpris = Mpris.get_default();
+
+  return (
+    <button
+      className={"musicPlayer"}
+      onClicked="astal -t music"
+      halign={Gtk.Align.START}
+      hasTooltip
+      tooltipText={"Music Player(s)"}
+      visible={bind(mpris, "players").as((players) => players.length > 0)}
+    >
+      <icon icon={"media-playback-start-symbolic"} />
+    </button>
+  );
+}
+
+function Taskbar() {
   const windowList = Variable<Window[]>([]);
   const windowListPrevious = Variable<Window[]>([]);
 
-  function checkSameWindowList(a: Window[], b: Window[]) {
-    return JSON.stringify(a) === JSON.stringify(b);
-  }
-
   interval(100, () => {
-    const _t = Tray.get_default();
-    const _ti = _t.get_items();
-
-    // Only update the trayItems if the items have changed
-    if (trayItemsPrevious.get().length !== _ti.length) {
-      trayItems.set(_ti);
-      trayItemsPrevious.set(_ti);
-    }
-
     const activeWindows = exec("wlrctl window list state:active")
       .split("\n")
       .map((w) => {
@@ -62,11 +104,127 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
       });
 
     if (!checkSameWindowList(windows, windowListPrevious.get())) {
-      print("Updating active windows");
       windowList.set(windows);
       windowListPrevious.set(windows);
     }
   });
+
+  return (
+    <box className={"taskbar"}>
+      {bind(windowList).as((windows) => {
+        return windows.map((window, index) => {
+          return (
+            <button
+              key={index}
+              onClicked={() => {
+                exec(`wlrctl window focus ${window.id}`);
+              }}
+              className={window.active ? "window active" : "window"}
+              hasTooltip
+              tooltipText={window.title}
+            >
+              <box halign={Gtk.Align.CENTER}>
+                <icon icon={iconReplacements(window.id)} className={"icon"} />
+              </box>
+            </button>
+          );
+        });
+      })}
+    </box>
+  );
+}
+
+function SysTray() {
+  const trayItems = Variable<Tray.TrayItem[]>([]);
+  const trayItemsPrevious = Variable<Tray.TrayItem[]>([]);
+
+  interval(100, () => {
+    const _t = Tray.get_default();
+    const _ti = _t.get_items();
+
+    // Only update the trayItems if the items have changed
+    if (trayItemsPrevious.get().length !== _ti.length) {
+      trayItems.set(_ti);
+      trayItemsPrevious.set(_ti);
+    }
+  });
+
+  return (
+    <box className={"tray"}>
+      {bind(trayItems).as((items) =>
+        items.map((item, index) => (
+          <menubutton
+            key={index}
+            className={"trayItem"}
+            tooltipMarkup={bind(item, "tooltipMarkup")}
+            usePopover={false}
+            actionGroup={bind(item, "actionGroup").as((ag) => ["dbusmenu", ag])}
+            menuModel={bind(item, "menuModel")}
+          >
+            <icon gicon={bind(item, "gicon")} />
+          </menubutton>
+        ))
+      )}
+    </box>
+  );
+}
+
+function Audio() {
+  const mpris = Mpris.get_default();
+  const volume = Variable(0).poll(100, () => {
+    const volOutput = exec("pactl get-sink-volume @DEFAULT_SINK@");
+    const vol = parseInt(volOutput.match(/(\d+)%/)?.[1] || "0", 10);
+    return vol;
+  });
+
+  return (
+    <button
+      className={"audio"}
+      onClicked="pavucontrol"
+      halign={Gtk.Align.START}
+      hasTooltip
+      tooltipText={"Volume Control"}
+      onScroll={(self, event) => {
+        print(event.delta_y);
+        if (event.delta_y > 0) {
+          exec("pactl set-sink-volume @DEFAULT_SINK@ -5%");
+        } else {
+          exec("pactl set-sink-volume @DEFAULT_SINK@ +5%");
+        }
+      }}
+    >
+      <box>
+        {bind(volume).as((v) => {
+          if (v > 0 && v <= 33) {
+            return <icon icon="audio-volume-low-symbolic" />;
+          } else if (v > 33 && v <= 66) {
+            return <icon icon="audio-volume-medium-symbolic" />;
+          } else if (v > 66) {
+            return <icon icon="audio-volume-high-symbolic" />;
+          } else {
+            return <icon icon="audio-volume-muted-symbolic" />;
+          }
+        })}
+        <label label={bind(volume).as((v) => `${v}%`)} />
+      </box>
+    </button>
+  );
+}
+
+function Clock() {
+  return (
+    <button
+      className={"clock"}
+      onClicked="astal -t calendar"
+      halign={Gtk.Align.END}
+    >
+      <label label={time()} />
+    </button>
+  );
+}
+
+export default function Bar(gdkmonitor: Gdk.Monitor) {
+  const { LEFT, RIGHT, BOTTOM } = Astal.WindowAnchor;
 
   return (
     <window
@@ -78,57 +236,17 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
       heightRequest={40}
     >
       <centerbox>
-        <button
-          className={"leftModule"}
-          onClicked="astal -t AppLauncher"
-          halign={Gtk.Align.START}
-        >
-          <icon icon={"view-grid"} />
-        </button>
+        <box className={"leftModule"} halign={Gtk.Align.START}>
+          <AppLauncherButton />
+          <MusicPlayer />
+        </box>
         <box className={"centerModule"} halign={Gtk.Align.CENTER}>
-          <box className={"taskbar"}>
-            {bind(windowList).as((windows) => {
-              return windows.map((window, index) => {
-                return (
-                  <button
-                    key={index}
-                    onClicked={() => {
-                      exec(`wlrctl window focus ${window.id}`);
-                      updateActiveWindows();
-                    }}
-                    className={window.active ? "window active" : "window"}
-                    hasTooltip
-                    tooltipText={window.title}
-                  >
-                    <box halign={Gtk.Align.CENTER}>
-                      <icon
-                        icon={iconReplacements(window.id)}
-                        className={"icon"}
-                      />
-                      {/* <label label={window.title} className={"title"} /> */}
-                    </box>
-                  </button>
-                );
-              });
-            })}
-          </box>
+          <Taskbar />
         </box>
         <box className={"rightModule"} halign={Gtk.Align.END}>
-          <box className={"tray"}>
-            {bind(trayItems).as((items) =>
-              items.map((item, index) => (
-                <button
-                  key={index}
-                  onClick={() => item.secondary_activate(0, 0)}
-                >
-                  <icon icon={item.iconName} />
-                </button>
-              ))
-            )}
-          </box>
-          <button onClicked="astal -t Calendar" halign={Gtk.Align.END}>
-            <label label={time()} />
-          </button>
+          <SysTray />
+          <Audio />
+          <Clock />
         </box>
       </centerbox>
     </window>
